bc88cf88cb10269b8beb23c037c4cf8155fe8ba3
Kuntal Danech <kuntal7398@gmail.com>
2021-04-28 14:00:36 +0530

    Spring boot jenkins with maven and git testing

--- src/main/java/com/danech/controller/MyRestController.java
+++ src/main/java/com/danech/controller/MyRestController.java
@@ -6,6 +6,6 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class MyRestController {
 	public ResponseEntity<String> welcome(){
-		return ResponseEntity.ok().body("Hello Spring Boot From Jenkins CICD");
+		return ResponseEntity.ok().body("Hello Spring Boot From Jenkins CICD, Testing");
 	}
 }
